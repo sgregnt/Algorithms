bcdbb5c69d0db6d030321113d1d5f7d2f9d83fa0
topnn <50776627+topnn@users.noreply.github.com>
2019-10-15 10:40:58 -0500

    porting from bitbucket

---
+++ DP/student_code.h
@@ -0,0 +1,21 @@
+//you can include standard C++ libraries here
+
+//required libraries
+#include <vector>
+
+
+// This function should return your name.
+// The name should match your name in Canvas
+
+void GetStudentName(std::string& your_name)
+{
+   your_name.assign("Gregory Naisat");
+}
+
+//This is the function you need to implement.
+int FindMaximumProfit(std::vector<int> w)
+{
+    //write your solution here
+
+    return /* your answer */;
+}
